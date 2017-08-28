d2b10483e19abc1fb3f1eac02c4cbc2d28dbe0e2
Mina Slater <slater.mina@gmail.com>
2017-08-28 15:50:17 -0500

    adds User model + name/email validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root to: "puppies#index"
   resources :puppies
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429041727) do
+ActiveRecord::Schema.define(version: 20170828201109) do
 
   create_table "puppies", force: :cascade do |t|
     t.string   "name",                       null: false
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170429041727) do
     t.boolean  "bored",      default: false, null: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
