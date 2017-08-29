3096bf4cc0dbc4c536394d2a01d375707cbeb0b5
Mina Slater <slater.mina@gmail.com>
2017-08-29 16:00:11 -0500

    adds email index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828201109) do
+ActiveRecord::Schema.define(version: 20170829204150) do
 
   create_table "puppies", force: :cascade do |t|
     t.string   "name",                       null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170828201109) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
