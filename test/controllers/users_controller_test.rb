56828fb0c92e18205b5f939875bfab7ca9db68be
Mina Slater <slater.mina@gmail.com>
2017-08-30 23:51:02 -0500

    adds create user controller method

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,8 +2,19 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    get new_user_url
     assert_response :success
   end
 
+  test "should create user" do
+    before_count = User.count
+    post users_url, params: { user: { name: "Aji", email: "user@example.com", password: "foobar", password_confirmation: "foobar" } }
+    assert_equal before_count + 1, User.count 
+  end
+
+  test "should not create user with incorrect params" do
+    before_count = User.count
+    post users_url, params: { user: { hair_color: "blue", species: "duck" } }
+    assert_equal before_count, User.count
+  end
 end
