17e338c56c7961929416a631c85c7d8dcbbc76dd
Mina Slater <slater.mina@gmail.com>
2017-09-02 18:32:41 -0500

    adds session for successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,9 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
     if @user
-      if BCrypt::Password.create(params[:password]) == @user.password_digest
-
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user.id)
       else
         flash[:alert] = "email/password combination does not match"
         redirect_back(fallback_location: new_session_path)
