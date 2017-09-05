a58e6d44450b958da715618e85160854811c3375
Mina Slater <slater.mina@gmail.com>
2017-09-05 14:07:28 -0500

    adds friendship model tests

--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class FriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup  
+    @aji = users(:test_user)
+    @mina = users(:test_user2)
+    @aji.friendship_one.create(friend_one_id: @mina.id)
+  end
+
+  test "should be able to add friends" do
+    assert_equal @aji.friend_ones.first, @mina
+    assert_equal @mina.friend_twos.first, @aji
+  end
+
+  test "should not create new friendship if duplicate" do
+    @mina.friendship_one.create(friend_one_id: @aji.id)
+    assert_equal @mina.friend_ones.count, 1
+  end
 end
