8b528afc25beca8cd37ec6d3173fc879a1a6412f
Mina Slater <slater.mina@gmail.com>
2017-09-19 15:10:35 -0500

    refactors #create to limit db calls

--- app/controllers/users/friendships_controller.rb
+++ app/controllers/users/friendships_controller.rb
@@ -5,12 +5,13 @@ class Users::FriendshipsController < ApplicationController
       redirect_to root_path
       return
     end
-    if @current_user.verify_friendship?(User.find(params[:user_id]))
+    user = User.find(params[:user_id])
+    if @current_user.verify_friendship?(user)
       flash[:alert] = "You are already friends!"
       redirect_to user_path(@current_user.id)
-    elsif @current_user != User.find(params[:user_id])
+    elsif @current_user != user
       @current_user.make_friends_with(User.find(params[:user_id]))
-      flash[:notice] = "Congrats! You are now friends with #{User.find(params[:user_id]).name}."
+      flash[:notice] = "Congrats! You are now friends with #{user.name}."
       redirect_to user_path(@current_user.id)
     end
   end
